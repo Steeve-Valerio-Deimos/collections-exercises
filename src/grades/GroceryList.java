a441e9f3a06a62fab5f67e562e0b766d68a54475
Eduardo Valerio Gamez <evaleriog@outlook.com>
2019-12-05 16:54:43 -0600

    Bonus in Grocery List App

--- src/grades/GroceryList.java
+++ src/grades/GroceryList.java
@@ -10,56 +10,91 @@ public class GroceryList {
         Scanner sc = new Scanner(System.in);
         ArrayList<Item> groceryList = new ArrayList<>();
 
-        System.out.println("Welcome and come to create a Grocery List!");
-        System.out.println("Would you like to create a Grocery List?");
+        System.out.println("\nWelcome and come to create a Grocery List!");
+        System.out.println("\nWould you like to create a Grocery List?(Y/N)");
         String response = sc.nextLine();
         response = response.trim();
 
-        do{
-            System.out.println("Choose from the following categories:");
-            System.out.println("1. Produce");
-            System.out.println("2. Dairy");
-            System.out.println("3. Deli");
-            int category = sc.nextInt();
-            sc.nextLine();
-            System.out.println("Enter the name of product");
-            String productName = sc.nextLine();
-            productName = productName.trim();
-            System.out.printf("How many of the %s do you need?%n", productName);
-            int amount = sc.nextInt();
-            Item item;
+        if(response.equalsIgnoreCase("y")) {
+            do {
+                System.out.println("Choose from the following categories:");
+                System.out.println("1. Produce");
+                System.out.println("2. Dairy");
+                System.out.println("3. Deli");
+                int category = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Enter the name of product");
+                String productName = sc.nextLine();
+                productName = productName.trim();
+                System.out.printf("How many of the %s do you need?%n", productName);
+                int amount = sc.nextInt();
+                Item item;
 
-           switch (category){
-               case 1:
-                   item = new Item(productName, "Produce", amount);
-                   break;
-               case 2:
-                   item = new Item(productName, "Dairy", amount);
-                   break;
-               case 3:
-                   item = new Item(productName, "Deli", amount);
-                   break;
-               default:
-                   item = new Item(productName, "Dairy", amount);
-                   break;
-           }
+                switch (category) {
+                    case 1:
+                        item = new Item(productName, "Produce", amount);
+                        break;
+                    case 2:
+                        item = new Item(productName, "Dairy", amount);
+                        break;
+                    case 3:
+                        item = new Item(productName, "Deli", amount);
+                        break;
+                    default:
+                        item = new Item(productName, "Dairy", amount);
+                        break;
+                }
 
-           groceryList.add(item);
+                groceryList.add(item);
 
-            System.out.println("What is the next step?");
-            System.out.println("1. Add Item.");
-            System.out.println("2. Complete list.");
-            int step = sc.nextInt();
+                System.out.println("What is the next step?");
+                System.out.println("1. Add Item.");
+                System.out.println("2. Complete list.");
+                int step = sc.nextInt();
 
-            if(step == 1){
-                response = "y";
-            }else{
-                response = "n";
+                if (step == 1) {
+                    response = "y";
+                } else {
+                    response = "n";
+                }
+
+            } while (response.equalsIgnoreCase("y"));
+
+            System.out.println("Do you want to print the completed list or by category?");
+            System.out.println("1. Complete List");
+            System.out.println("2. By Category");
+            int choice = sc.nextInt();
+
+            if (choice == 1) {
+                printArray(groceryList);
+            } else if (choice == 2) {
+                System.out.println("Choose from the following categories:");
+                System.out.println("1. Produce");
+                System.out.println("2. Dairy");
+                System.out.println("3. Deli");
+                int catChoice = sc.nextInt();
+
+                switch (catChoice) {
+                    case 1:
+                        printCategory(groceryList, "Produce");
+                        break;
+                    case 2:
+                        printCategory(groceryList, "Dairy");
+                        break;
+                    case 3:
+                        printCategory(groceryList, "Deli");
+                        break;
+                    default:
+                        System.out.println("Incorrect Category...Displaying all items.");
+                        printArray(groceryList);
+                }
             }
 
-        }while(response.equalsIgnoreCase("y"));
+        }else {
+            System.out.println("Thank you for using the Grocery List App...");
+            System.out.println("Have a nice day...");
+        }
 
-        printArray(groceryList);
     }
 
     public static void printArray(ArrayList<Item> list){
@@ -68,6 +103,16 @@ public class GroceryList {
             for(int i=0; i < list.size(); i++){
                 System.out.printf("| %s | %s | %d |%n",list.get(i).name,list.get(i).category, list.get(i).amount);
             }
+        }
+    }
+
+    public static void printCategory(ArrayList<Item> list, String category){
+        Collections.sort(list, Item.compareName);
+        for(int i=0; i < list.size(); i++){
+            if(list.get(i).category.equals(category)){
+                System.out.printf("| %s | %s | %d |%n",list.get(i).name,list.get(i).category, list.get(i).amount);
+            }
+
         }
     }
 }
